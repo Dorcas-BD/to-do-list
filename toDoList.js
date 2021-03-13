3c7affd48952897315ddb01eb0444fbe41e9daf3
Dorcas-BD <52034693+Dorcas-BD@users.noreply.github.com>
2021-03-13 23:39:27 +0100

    first commit

---
+++ toDoList.js
@@ -0,0 +1,83 @@
+var myNodelist = document.getElementsByTagName("LI"); 
+
+
+var i; for (i = 0; i < myNodelist.length; i++) { 
+    var span = document.createElement("SPAN"); 
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close"; span.appendChild(txt);
+    myNodelist[i].appendChild(span); 
+  } 
+  
+// Click on a close button to hide the current list item 
+    var close = document.getElementsByClassName("close");
+    var i; for (i = 0; i < close.length; i++) { 
+    close[i].onclick = function() { 
+        var div = this.parentElement; div.style.display = "none"; 
+    } 
+} 
+
+// Add a "checked" symbol when clicking on a list item 
+
+    var list = document.querySelector('ul');
+    list.addEventListener('click',function(ev) { 
+           if (ev.target.tagName === 'LI') { 
+           ev.target.classList.toggle('checked'); 
+         } 
+     }, false); 
+
+
+
+// Create a new list item when clicking on the "Add" button 
+
+    function newElement() { 
+        var li = document.createElement("li"); 
+        var inputValue = document.getElementById("myInput").value;
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t); 
+        
+        if (inputValue === '') { 
+            alert("You must write something!"); 
+        } else {  document.getElementById("myUL").appendChild(li); 
+}
+
+var inputValue = document.getElementById("myInput")
+inputValue.addEventListener("keypress", function setKey(e){
+    if (e.which == 13 || e.keycode == 13){
+    // If (e.which == 13 || e.keycode == 13)
+
+    event.preventDefault();
+      //Trigger the button element with a click
+    // document.querySelector('.button').click();
+    }
+    })
+// inputValue.addEventListener("keypress", function(event) {
+//     //Number 13 is the "Enter" key on the keyboard
+//     if (event.keyCode === 13) {
+//       // Cancel the default action, if needed
+//       event.preventDefault();
+//       //Trigger the button element with a click
+//       document.querySelector('.button').click();
+//     }
+//   });
+
+
+        document.getElementById("myInput").value = ""; 
+       var span = document.createElement("SPAN"); 
+       var txt = document.createTextNode("\u00D7");
+       span.className = "close";
+       span.appendChild(txt);
+       li.appendChild(span); 
+       
+       for (i = 0; i < close.length; i++) { 
+           close[i].onclick = function() { 
+               var div = this.parentElement; 
+               div.style.display = "none"; 
+           } 
+       } 
+   } 
+//Clearing the list 
+
+function removeAll(){ 
+    var lst = document.getElementsByTagName("ul");
+    lst[0].innerHTML = ""; 
+}
